093b751a4272328809fd344c236c9feffa1b62c5
Omri1287 <omrsh08@gmail.com>
2022-12-21 17:46:30 +0200

    add files

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const charactersAndSymbals = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]; 
+
+let keyBox1 = document.getElementById("key-box1")
+let keyBox2 = document.getElementById("key-box2")
+let passButton = document.getElementById("passButton")
+let numLength = document.getElementById("num-input")
+
+function randomChar(){
+    let randomNum = Math.floor(Math.random()*characters.length)
+    return characters[randomNum]
+}
+passButton.addEventListener("click", function(){
+    console.log("9")
+    keyBox1.textContent = "               "
+    keyBox2.textContent = "               "
+    let i=0
+    while (i<=numLength.length){
+        i++;
+        keyBox1.textContent += randomChar()
+        keyBox2.textContent += randomChar()
+    }
+})
